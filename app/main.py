d114fb4e8b26fe874732a60b7f3c9416cc698bac
magneto903 <31848594+MAGNETO903@users.noreply.github.com>
2023-10-14 09:04:41 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,8 @@
 # import
 import uvicorn
 from fastapi import FastAPI
+
 from langchain.llms import LlamaCpp
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.document_loaders import PyPDFLoader
-from langchain.document_loaders.merge import MergedDataLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.prompts import PromptTemplate
-from langchain.chains import RetrievalQA
 from huggingface_hub import hf_hub_download
 
 import time
@@ -20,39 +13,6 @@ print(time.ctime(), "app launched")
 
 
 # read, parse and split documents
-root_dir = './'
-
-# PDFs
-pdf_loader_1 = PyPDFLoader(root_dir+'documents/doc1.pdf')
-pdf_loader_2 = PyPDFLoader(root_dir+'documents/doc2.pdf')
-
-# csv
-csv_loader = CSVLoader(
-    file_path=root_dir+'documents/cards.csv',
-    encoding="utf-8",
-    csv_args={'delimiter': ','}
-)
-
-loader_all = MergedDataLoader(loaders=[
-    csv_loader,
-    pdf_loader_1,
-    pdf_loader_2
-])
-
-documents = loader_all.load()
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200
-)
-
-texts = text_splitter.split_documents(documents)
-print(time.ctime(), "documents loaded")
-# creating vector DB
-
-embedder = HuggingFaceEmbeddings(model_name='intfloat/multilingual-e5-large')
-vec_db = FAISS.from_documents(texts, embedder)
-print(time.ctime(), "vector DB ready")
 
 # our LLM
 model_name_or_path = "TheBloke/Llama-2-7b-Chat-GGUF"
@@ -68,32 +28,11 @@ llm = LlamaCpp(
     top_p=0.9, # Verbose is required to pass to the callback manager
     lang="ru",
 )
-print(time.ctime(), "llm ready")
-# building final chain
-prompt_template = """
-Используйте следующие фрагменты контекста, чтобы ответить на вопрос в конце.
-{context}
-Вопрос: {question}
-Полезный ответ:"""
 
-PROMPT = PromptTemplate(
-    template=prompt_template,
-    input_variables=["context", "question"]
-)
-
-qa_chain = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type='stuff',
-    retriever=vec_db.as_retriever(),
-    chain_type_kwargs={"prompt": PROMPT}
-)
-
-print(time.ctime(), "chain ready")
-app = FastAPI()
 @app.get("/message")
 def message(user_id: str, message: str):
-    llm_response = qa_chain({"query": message})
-    return {"message": llm_response['result']}
+    
+    return {"message": llm(mesage)}
 
 if __name__ == '__main__':
     uvicorn.run(app)
